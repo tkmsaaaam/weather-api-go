54e785d5be5495cb837368f4be20f7ee8e106620
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-09-17 21:56:11 +0900

    add test

--- weather_test.go
+++ weather_test.go
@@ -1,1 +1,44 @@
-package weather
+package weather_test
+
+import (
+	"fmt"
+	"github.com/dnaeon/go-vcr/recorder"
+	"github.com/stretchr/testify/assert"
+	"github.com/tkmsaaaam/weather-api-go"
+	"net/http"
+	"testing"
+)
+
+func TestGetOk(t *testing.T) {
+	recorderClient, _ := recorder.New("./fixtures/ok")
+	defer func(recorderClient *recorder.Recorder) {
+		err := recorderClient.Stop()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(recorderClient)
+	weatherClient := weather.Client{Client: &http.Client{
+		Transport: recorderClient,
+	}}
+	t.Run("Get", func(t *testing.T) {
+		response, _ := weatherClient.Get("130010")
+		assert.Equal(t, "東京都 東京 の天気", response.Title)
+	})
+}
+
+func TestGetErr(t *testing.T) {
+	recorderClient, _ := recorder.New("./fixtures/err")
+	defer func(recorderClient *recorder.Recorder) {
+		err := recorderClient.Stop()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(recorderClient)
+	weatherClient := weather.Client{Client: &http.Client{
+		Transport: recorderClient,
+	}}
+	t.Run("Get", func(t *testing.T) {
+		response, _ := weatherClient.Get("400000")
+		assert.Equal(t, "", response.Title)
+	})
+}
