99cd6c8d8aa9d1de0f1e2b576eee9e2373c39b93
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-09-17 13:54:59 +0900

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tkmsaaaam/weather-api-go
+
+go 1.19
---
+++ weather.go
@@ -0,0 +1,93 @@
+package weather
+
+import (
+	"time"
+)
+
+type Description struct {
+	PublicTime          time.Time `json:"publicTime"`
+	PublicTimeFormatted string    `json:"publicTimeFormatted"`
+	HeadlineText        string    `json:"headlineText"`
+	BodyText            string    `json:"bodyText"`
+	Text                string    `json:"text"`
+}
+
+type Detail struct {
+	Weather string `json:"weather"`
+	Wind    string `json:"wind"`
+	Wave    string `json:"wave"`
+}
+
+type DetailTemperature struct {
+	Celsius    string `json:"celsius"`
+	Fahrenheit string `json:"fahrenheit"`
+}
+
+type Temperature struct {
+	Min DetailTemperature `json:"min"`
+	Max DetailTemperature `json:"max"`
+}
+
+type ChanceOfRain struct {
+	T0006 string `json:"T00_06"`
+	T0612 string `json:"T06_12"`
+	T1218 string `json:"T12_18"`
+	T1824 string `json:"T18_24"`
+}
+
+type Image struct {
+	Title  string `json:"title"`
+	URL    string `json:"url"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+}
+
+type Forecast struct {
+	Date         string       `json:"date"`
+	DateLabel    string       `json:"dateLabel"`
+	Telop        string       `json:"telop"`
+	Detail       Detail       `json:"detail"`
+	Temperature  Temperature  `json:"temperature"`
+	ChanceOfRain ChanceOfRain `json:"chanceOfRain"`
+	Image        Image        `json:"image"`
+}
+
+type Location struct {
+	Area       string `json:"area"`
+	Prefecture string `json:"prefecture"`
+	District   string `json:"district"`
+	City       string `json:"city"`
+}
+
+type CopyrightImage struct {
+	Title  string `json:"title"`
+	Link   string `json:"link"`
+	URL    string `json:"url"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+}
+
+type Provider struct {
+	Link string `json:"link"`
+	Name string `json:"name"`
+	Note string `json:"note"`
+}
+
+type Copyright struct {
+	Title    string         `json:"title"`
+	Link     string         `json:"link"`
+	Image    CopyrightImage `json:"image"`
+	Provider []Provider     `json:"provider"`
+}
+
+type Response struct {
+	PublicTime          time.Time   `json:"publicTime"`
+	PublicTimeFormatted string      `json:"publicTimeFormatted"`
+	PublishingOffice    string      `json:"publishingOffice"`
+	Title               string      `json:"title"`
+	Link                string      `json:"link"`
+	Description         Description `json:"description"`
+	Forecasts           []Forecast  `json:"forecasts"`
+	Location            Location    `json:"location"`
+	Copyright           Copyright   `json:"copyright"`
+}
